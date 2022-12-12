0e69295a17f0ac89f7c07c50537fac80e25e068c
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2022-12-12 19:21:11 +0100

    commita

---
+++ src/app/equipe/equipe.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EquipeRoutingModule } from './equipe-routing.module';
+import { ListeEquipeComponent } from './liste-equipe/liste-equipe.component';
+import { AddEquipeComponent } from './add-equipe/add-equipe.component';
+import { UpdateEquipeComponent } from './update-equipe/update-equipe.component';
+
+
+@NgModule({
+  declarations: [
+    ListeEquipeComponent,
+    AddEquipeComponent,
+    UpdateEquipeComponent
+  ],
+  imports: [
+    CommonModule,
+    EquipeRoutingModule
+  ]
+})
+export class EquipeModule { }
